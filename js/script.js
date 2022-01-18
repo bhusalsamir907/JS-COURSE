e634fecb02bc1ce81fc338525b01380cc2c42317
Samir Bhusal <bhusalsamir907@gmail.com>
2022-01-18 13:56:57 +0545

    learning js function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
 document.write("Writing js code");
- let marks={
-     ravi : 90,
-     shubham:80,
-     harry:99
- };
+let marks = {
+    ravi: 90,
+    shubham: 80,
+    harry: 99
+};
 
- document.getElementById("para1").innerHTML=marks.harry;
+function paraChnge() {
+    document.getElementById('spn').innerHTML = "This is first paragraph from JS";
+}
+
+
+document.getElementById("para1").innerHTML = marks.harry;
+
+//  Functions in JS
+function myFunction(a, b) {
+    return a * b;
+}
+
+document.getElementById("para2").innerHTML = myFunction(10, 20);
+
+//  Avg function
+function avg(a, b) {
+    return (a + b) / 2;
+
+}
+c1=avg(20, 10);
+console.log(c1);
